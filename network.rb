5463234a692875de4ac163290a34492821690d01
rkass <rck72@cornell.edu>
2012-10-18 10:52:15 -0400

    Propagate added, some transition functions as well

--- network.rb
+++ network.rb
@@ -4,27 +4,48 @@ class Network
 
   attr_accessor :nodes
   
-  # creates a fully connected network with 
-  # n nodes per layer, i input nodes, h hidden layers
+  # creates a fully connected network (layer i only outputs to i + 1)
+  # with n nodes per layer, i input nodes, h hidden layers, and o output nodes
+  # transition function tf--a number--corresponding values available for lookup
+  # in Node.calculate
   # each index of nodes represents a different layer
-  def initialize(n, i, h)
+  def initialize(n, i, h, o, tf)
     @nodes = []
     #input layer
     @nodes[0] = []
     (0..(i - 1)).each do
-      @nodes[0].push Node.new(nil, nil)
+      @nodes[0].push Node.new(nil, nil, tf)
     end
     #hidden layers
     (1..h).each do
       @nodes[h] = []
       (0..(n - 1)).each do
-        @nodes[h].push Node.new(@nodes[h - 1], initWeights(@nodes[h - 1].count))
+        @nodes[h].push Node.new(@nodes[h - 1], initWeights(@nodes[h - 1].count), tf)
       end
     end
     #output layer
-    @nodes[h + 1] = [Node.new(@nodes[h], initWeights(@nodes[h].count))]
+    @nodes[h + 1] = []
+    (0..(o - 1)).each do
+      @nodes[h + 1].push Node.new(@nodes[h], initWeights(@nodes[h].count), tf)
+    end
    end 
   
+  def propagate(input)
+    raise "Input array not same length as amount of nodes in input layer" 
+      unless input.count == @nodes[0].count
+    #set inputs
+    for i in 0..(input.count - 1)
+      @nodes[i].input = input[i]
+    end
+    #prop
+    for i in 0..(@nodes.count - 1)
+      for n in @nodes[i]
+        n.calculate
+      end
+    end
+  end
+    
+
   #creates an array of n random numbers from 0 to 1
   def initWeights(n)
     ret = []
--- node.rb
+++ node.rb
@@ -1,10 +1,40 @@
 class Node
 
-  attr_accessor :incomingNodes, :incomingWegihts, :input
+  attr_accessor :incomingNodes, :incomingWegihts, :input, :output
 
-  def initialize(nodes, weights)
+  def initialize(nodes, weights, transition)
     @incomingNodes = nodes #nodes receiving this as input
     @incomingWeights = weights #weights 
+    @transitionFunction = transition
   end
+
+  def calculate
+    if @input == nil
+      input = sumWithWeights
+      case @transitionFunction
+      when 0
+        @output = logistic input
+      end
+    else
+      @output = @input
+    end
+  end 
+
+  def sumWithWeights
+    ret = 0
+    for i in 0..(incomingNodes.count - 1)
+      ret += incomingNodes[i].output * incomingWeights[i]
+    end
+    ret
+  end
+    
+  #Transition Functions:
+  
+  def logistic(x)
+    1/(1 + Math.exp(-x))
+  end
+
+  private :sumWithWeights, :logistic
+    
 
 end
