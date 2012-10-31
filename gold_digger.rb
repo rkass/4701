ba39db1e7b1787709d725efb5819d57a71f22205
rkass <rck72@cornell.edu>
2012-10-30 20:20:48 -0400

    x

---
+++ gold_digger.rb
@@ -0,0 +1,18 @@
+require File.expand_path('../trainNetwork.rb', __FILE__)
+
+#loops over trainAndValidate to find the best result
+#returns the best neural net, along with it's squared
+#difference average, and the corresponding naive
+#squared difference average
+#inputs correspond
+def findBestIn(x, n, h, step, loops)
+  #set initial
+  best = trainAndValidate(x, n, h, step)
+  for i in 2..loops
+    trained = trainAndValidate(x, n, h, step)
+    if trained.ann < best.ann
+      best = trained
+    end
+  end
+  best
+end
---
+++ trainNetwork.rb
@@ -0,0 +1,61 @@
+require File.expand_path('../network.rb', __FILE__)
+require File.expand_path('../basics.rb', __FILE__)
+require 'ostruct'
+
+#x = |training set|(|training set| + |validation set|)
+#n = number of nodes per hidden layer
+#h = number of hidden layers
+#step = learning rate
+def trainAndValidate(x, n, h, step)
+  basics = Basics.new
+  trainingSet = basics.getFirstSnapshotsRatio x
+  validationSet = basics.getLastSnapshotsRatio x
+  expectedArray = getExpected trainingSet
+  trainingSet = trainingSet.slice(0..(trainingSet.count - 25))
+  network = Network.new(n, 4, h, 1, 0, step)
+  network.train(trainingSet, expectedArray)
+  squaredDiffs = getSquaredDiffs(network, validationSet)
+  ret = OpenStruct.new
+  ret.ann = average squaredDiffs.netResults
+  ret.naive = average squaredDiffs.naiveResults
+  ret.net = network
+  ret
+end
+
+def getSquaredDiffs(network, validationSet)
+  resultsOnValidation = [] 
+  naiveResults = [] 
+  for i in 0..(validationSet.count - 25)
+    network.propagate validationSet[i]
+    #push squared difference of ANN result for 24h from i
+    resultsOnValidation.push ((network.nodes[network.nodes.count - 1][0].output - validationSet[i + 24][1]) ** 2)
+    #push square difference obtained by predicting result strictly from the last price
+    naiveResults.push ((validationSet[i][1] - validationSet[i + 24][1]) ** 2)
+  end
+  ret = OpenStruct.new
+  ret.netResults = resultsOnValidation
+  ret.naiveResults = naiveResults
+  ret
+end
+
+def getExpected(trainingSet)
+  expected = []
+  for i in 24..(trainingSet.count - 1)
+    expected[i - 24] = [trainingSet[i][1]]
+  end
+  expected
+end
+
+def average(arr)
+  tot = 0.0
+  for i in arr
+    tot += i
+  end
+  tot/arr.count
+end
+
+def lostOutput input
+  node = Node.new(nil, nil, 0, false, false)
+  print "Logistic: #{node.logistic input}"
+  print "Derivative: #{node.logisticDeriv input}"
+end
